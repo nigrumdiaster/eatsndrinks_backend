8721bd9c54a9dbdf3c4c3bc3022bfeda0ad5f439
nigrumdiaster <119689847+nigrumdiaster@users.noreply.github.com>
2024-10-16 03:44:54 +0700

    Update model

--- backend/eatsndrinks/reviews/models.py
+++ backend/eatsndrinks/reviews/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from catalogue.models import Product
 from users.models import User
 
 # Create your models here.
@@ -13,5 +14,7 @@ class Review(models.Model):
     content_object = GenericForeignKey('content_type', 'object_id')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
     def __str__(self):
         return f'Review by {self.user.username} for {self.content_object}'
