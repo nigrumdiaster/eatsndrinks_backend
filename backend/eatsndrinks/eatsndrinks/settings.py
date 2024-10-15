cedd238f4427a3b559a3aef07a985f40f01f39c5
nigrumdiaster <119689847+nigrumdiaster@users.noreply.github.com>
2024-10-16 03:45:09 +0700

    Add basic auth

--- backend/eatsndrinks/eatsndrinks/settings.py
+++ backend/eatsndrinks/eatsndrinks/settings.py
@@ -152,6 +152,7 @@ REST_FRAMEWORK = {
     # ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 
