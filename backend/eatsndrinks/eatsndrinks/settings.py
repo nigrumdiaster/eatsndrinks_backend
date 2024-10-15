2c86e13535e04fc8f6cea01ca08e7ec907bc6692
nigrumdiaster <119689847+nigrumdiaster@users.noreply.github.com>
2024-10-16 05:42:50 +0700

    Updare database

--- backend/eatsndrinks/eatsndrinks/settings.py
+++ backend/eatsndrinks/eatsndrinks/settings.py
@@ -93,10 +93,19 @@ WSGI_APPLICATION = "eatsndrinks.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'eatsndrinks',  
+        'USER': 'root',  
+        'PASSWORD': '123456',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'TIME_ZONE': 'Asia/Saigon',
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        
+        }  
+    }  
 }
 
 
