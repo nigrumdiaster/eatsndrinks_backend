f86f4aefe071b38dd7477e211f7ac6f05a270cad
Đại Nghĩa <nigrumdiaster@gmail.com>
2025-03-22 22:51:40 +0700

    Update order detail api

--- eatsndrinks/order/urls.py
+++ eatsndrinks/order/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("create", OrderCreateView.as_view(), name="user-order-create"),
     path("admin/orders/", AdminOrderView.as_view(), name="admin-order-list"),  # GET danh sách
     path("admin/order/<int:id>/", AdminOrderDetailView.as_view(), name="admin-order-detail"),  # GET, PUT, PATCH, DELETE
-    path("<int:order_id>/items/", OrderItemsView.as_view(), name="order-items"),
+    path("<int:order_id>/", OrderItemsView.as_view(), name="order-items"),
 ]
